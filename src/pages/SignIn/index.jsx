fcad78e62fd5dd81f463abf138e1520f6b27909d
Mewerton Melo <mewerton@gmail.com>
2022-11-25 18:17:41 -0300

    Page SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,10 @@
 import { Container, Form } from './styles'
-import Logo from '../../assets/logoblue.svg'
 import { Input } from '../../components/Input'
 import {FiMail} from 'react-icons/fi'
 import {BsPerson} from 'react-icons/bs'
 import {RiLockPasswordLine} from 'react-icons/ri'
+import { Button } from '../../components/Button'
+import LogoGG from '../../assets/logoblueGG.svg'
 
 
 
@@ -11,34 +12,40 @@ import {RiLockPasswordLine} from 'react-icons/ri'
 export function SignIn(){
     return(
         <Container>
+            <div className="pageSignIn">
+
             <div className="logo">
-                <img src={Logo} alt="Logo"/>
+                <img src={LogoGG} alt="Logo" />
                 <span>food explorer</span>
             </div>
-            
+
             <Form>
-                <h1>Faça login</h1>
-                <span>Seu nome</span>
-                <Input
-                    placeholder="Exemplo: Maria da Silva"
-                    type="text"
-                    icon={BsPerson}
-                />
-                <span>E-mail</span>
-                <Input
-                    placeholder="Exemplo: exemplo@exemplo.com.br"
-                    type="text"
-                    icon={FiMail}
-                />
-                <span>Senha</span>
-                <Input
-                    placeholder="No mínimo 6 caracteres"
-                    type="text"
-                    icon={RiLockPasswordLine}
-                />
-                
-             
+                <div className="login">
+                    <h1>Faça login</h1>
+                    <div className="email">
+                    <span>E-mail</span>
+                    <Input
+                        placeholder="Exemplo: exemplo@exemplo.com.br"
+                        type="text"
+                        icon={FiMail}
+                    />
+                    </div>
+                    <div className="password">
+                    <span>Senha</span>
+                    <Input
+                        placeholder="No mínimo 6 caracteres"
+                        type="password"
+                        icon={RiLockPasswordLine}
+                    />
+                    </div>
+                    <Button name="Entrar"/>
+                    <div className="create">
+                        <a href="#">Criar uma conta</a>
+                    </div> 
+                </div>
+
             </Form>
+            </div>
         </Container>
     )
 }
