712743cef9b1e913d64661c28d30c968f02b0cbf
Mewerton Melo <mewerton@gmail.com>
2022-11-30 13:08:55 -0300

    Textarea

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,15 @@
 import { Container } from "./styles";
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
+import { Textarea } from '../../components/Textarea'
+import { Input } from '../../components/Input'
 
 export function New(){
     return(
         <Container>
             <Header/>
+            <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"/>
+            <Input placeholder="Ex.: Salada Ceasar"/>
             
             
         </Container>
