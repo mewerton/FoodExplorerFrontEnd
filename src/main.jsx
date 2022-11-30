4c5b64bc75d63580f306f2159358bb9def85365d
Mewerton Melo <mewerton@gmail.com>
2022-11-30 12:26:41 -0300

    New

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,15 @@ import theme from './styles/theme'
 //import { Details } from './pages/Details'
 //import { SignIn } from './pages/SignIn'
 //import { SignUp } from './pages/SignUp'
-import { Product } from './pages/Product'
+//import { Product } from './pages/Product'
+import { New } from './pages/New'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Product />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
