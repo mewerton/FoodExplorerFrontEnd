2204d498faf41a0ade73eaca28ca79d6fa7c9f93
Mewerton Melo <mewerton@gmail.com>
2022-12-09 19:37:15 -0300

    BrowserRouter
    Tentando resolver problema da rota, preciso fazer verificação se o usuário é admin, se sim então manda pra rota do admin, se não então manda para roda normal.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 import { AdminRoutes } from "./admin.routes";
 
 export function Routes(){
+
+    const { user } = useAuth()
+
+
     return(
         <BrowserRouter>
-            <AuthRoutes/>
+            {user ? <AppRoutes/> : <AuthRoutes/>}
+            {/* {response.data.user.admin = 1 ? <AdminRoutes/> : <AuthRoutes/>} */}
+
         </BrowserRouter>
     )
 }
